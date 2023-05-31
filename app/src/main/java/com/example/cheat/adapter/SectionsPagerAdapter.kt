44abe6132c7d0655800ddcb1ada01116b7e454db
nadhifnabhan18 <nadhifnabhan18@gmail.com>
2023-05-31 08:39:03 +0700

    On Boarding Screen Fix #1

---
+++ app/src/main/java/com/example/cheat/adapter/SectionsPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.cheat.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.cheat.ui.OnBoardingFirstFragment
+import com.example.cheat.ui.OnBoardingSecondFragment
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount() = ITEM_COUNT
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment: Fragment = when (position) {
+            0 -> OnBoardingFirstFragment()
+            1 -> OnBoardingSecondFragment()
+            else -> throw IllegalArgumentException("Invalid Fragment Position + $position")
+        }
+        return fragment
+    }
+
+    private companion object {
+        const val ITEM_COUNT = 2
+    }
+}
