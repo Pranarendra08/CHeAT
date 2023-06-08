c52713e25e62edc166a1d8964b158fa1eeb48bd7
Pranarendra08 <pranarendra@gmail.com>
2023-06-08 21:51:28 +0700

    Update navigation

--- app/src/main/java/com/example/cheat/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/cheat/ui/register/RegisterActivity.kt
@@ -1,13 +1,30 @@
 package com.example.cheat.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cheat.R
+import com.example.cheat.databinding.ActivityRegisterBinding
+import com.example.cheat.ui.login.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.apply {
+            //logika cek nama, email, dan password
+
+            btnRegister.setOnClickListener {
+                startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            }
+        }
     }
 
 }
